6cb7ae330c96d8e5bcf7a22b50e55a83151a1be9
jahangir2002 <jahangirshaikh553@gmail.com>
2025-02-03 16:11:24 +0530

    Add Search component and SVG icons for search and star functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div className='text-5xl font-bold underline'>App</div>
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+
+        <header>
+          <img className='h-48 w-96 object-cover' src="./hero.png" alt="Hero banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}
+         />
+
+      </div>
+    </main>
   )
 }
 
