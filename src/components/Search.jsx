d95a748e0d4a6cdf37fc547599d8d90575869e75
jahangir2002 <jahangirshaikh553@gmail.com>
2025-02-05 10:46:33 +0530

    Enhance Search component with input field and search icon for movie queries

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Search = ({searchTerm, setSearchTerm}) => {
   return (
-    <div className='text-white'>Search</div>
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt="search" />
+        <input type="text" 
+        placeholder='Search for a movie...'
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
   )
 }
 
